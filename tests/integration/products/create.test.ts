011a9ec7a29b1c505d48d37266af17dfa6db4e6a
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2023-09-13 20:54:25 -0300

    test: req 1 - test de product controller, services e productRoute

--- tests/integration/products/create.test.ts
+++ tests/integration/products/create.test.ts
@@ -3,6 +3,7 @@ import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 import createProduct from '../../mocks/createProduct.mock'
 import app from '../../../src/app'
+import ProductModel from '../../../src/database/models/product.model';
 
 chai.use(chaiHttp);
 
@@ -10,9 +11,11 @@ describe('POST /products', function () {
   beforeEach(function () { sinon.restore(); });
 
   it('Retorna status 201 e dados do produto cadastrado', async function() {
+    const dataFromDB = ProductModel.build(createProduct.returnDB);
+    sinon.stub(ProductModel, 'create').resolves(dataFromDB);
     const response = await chai.request(app).post('/products').send(createProduct.bodyRequest);
 
     expect(response.status).to.be.equal(201);
-    // expect(response.body).to.deep.equal(createProduct.serviceReturn.data); 
+    expect(response.body).to.deep.equal(createProduct.serviceReturn.data); 
   })
 });
